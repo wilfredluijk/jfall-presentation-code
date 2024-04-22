6b7fad81c4811ed0d43ccc671c60b5778147b252
Wilfred Luijk <wilfred.luijk@topicus.nl>
2024-04-22 20:14:52 +0200

    Change annotation in QuizSocketManager from Component to Controller
    The annotation in the QuizSocketManager class has been changed from @Component to @Controller. This conversion better represents the role of the class in handling the incoming web request and forms part of the core annotation suite used for designing web applications in Spring MVC.

--- src/main/java/nl/profit4cloud/hero/quiz/QuizSocketManager.java
+++ src/main/java/nl/profit4cloud/hero/quiz/QuizSocketManager.java
@@ -14,14 +14,14 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Controller
 public class QuizSocketManager {
 
     private final QuizManager quizRoomManager;
