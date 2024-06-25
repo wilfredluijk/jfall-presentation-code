6763943ef6b644b1e69244244b345e6e8c4163b9
Wilfred Luijk <wilfred.luijk@topicus.nl>
2024-06-25 20:25:28 +0200

    Revert "Refactor LeaderboardEntry to simple Java class"
    This reverts commit 3b40341a2050ee8dcaebf814a232541ab61d5575.

--- src/main/java/nl/profit4cloud/hero/leaderboard/LeaderboardEntry.java
+++ src/main/java/nl/profit4cloud/hero/leaderboard/LeaderboardEntry.java
@@ -1,81 +1,22 @@
 package nl.profit4cloud.hero.leaderboard;
 
-import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
 public class LeaderboardEntry {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
     private String name;
     private Integer contestantId;
     private Integer score;
     private Long correctQuestions;
     private Integer totalQuestions;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getContestantId() {
-        return contestantId;
-    }
-
-    public void setContestantId(Integer contestantId) {
-        this.contestantId = contestantId;
-    }
-
-    public Integer getScore() {
-        return score;
-    }
-
-    public void setScore(Integer score) {
-        this.score = score;
-    }
-
-    public Long getCorrectQuestions() {
-        return correctQuestions;
-    }
-
-    public void setCorrectQuestions(Long correctQuestions) {
-        this.correctQuestions = correctQuestions;
-    }
-
-    public Integer getTotalQuestions() {
-        return totalQuestions;
-    }
-
-    public void setTotalQuestions(Integer totalQuestions) {
-        this.totalQuestions = totalQuestions;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        LeaderboardEntry that = (LeaderboardEntry) o;
-        return Objects.equals(name, that.name) && Objects.equals(contestantId, that.contestantId) && Objects.equals(score, that.score) && Objects.equals(correctQuestions, that.correctQuestions) && Objects.equals(totalQuestions, that.totalQuestions);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hashCode(name);
-        result = 31 * result + Objects.hashCode(contestantId);
-        result = 31 * result + Objects.hashCode(score);
-        result = 31 * result + Objects.hashCode(correctQuestions);
-        result = 31 * result + Objects.hashCode(totalQuestions);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "LeaderboardEntry{" +
-                "name='" + name + '\'' +
-                ", contestantId=" + contestantId +
-                ", score=" + score +
-                ", correctQuestions=" + correctQuestions +
-                ", totalQuestions=" + totalQuestions +
-                '}';
-    }
 }
